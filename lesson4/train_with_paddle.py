3a9a3ac1c126b61b814ae06113285ba405af4f48
hui <897744517@qq.com>
2017-11-30 12:03:47 +0800

    change codes

--- lesson4/train_with_paddle.py
+++ lesson4/train_with_paddle.py
@@ -6,23 +6,23 @@
 #
 ################################################################################
 """
-Authors: Jiahui Liu(wx_crome@163.com)
-Date:    2017/11/27 16:26:06
+Authors: Jiahui Liu(2505774110@qq.com)
+Date:    2017/11/17 17:27:06
 
-使用paddle框架实现浅层神经网络解决“花”型图案分类问题，关键步骤如下：
+使用paddle框架实现浅层神经网络识别“花，型图案，关键步骤如下：
 1.载入数据和预处理：load_data()
 2.初始化
 3.配置网络结构
 4.定义成本函数cost
 5.定义优化器optimizer
-6.定义reader()分别用于读取训练数据和测试数据
+6.定义两个reader()分别用于读取训练数据和测试数据
 7.预测并测试准确率train_accuracy和test_accuracy
 """
-
 
 import matplotlib
 import numpy as np
 import paddle.v2 as paddle
+
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
@@ -56,18 +56,15 @@ def load_data():
     TRAINING_SET = np.hstack((train_set_x.T, train_set_y.T))
     TEST_SET = np.hstack((test_set_x.T, test_set_y.T))
 
-
-# 训练数据集
-def train():
+# 读取训练数据或测试数据，服务于train()和test()
+def read_data(data_set):
     """
-    定义一个reader来获取训练数据集及其标签
-
-    Args:
-    Return:
-        reader -- 用于获取训练数据集及其标签的reader
+        一个reader
+        Args:
+            data_set -- 要获取的数据集
+        Return:
+            reader -- 用于获取训练数据集及其标签的生成器generator
     """
-    global TRAINING_SET
-
     def reader():
         """
         一个reader
@@ -76,35 +73,63 @@ def train():
             data[:-1], data[-1:] -- 使用yield返回生成器(generator)，
                     data[:-1]表示前n-1个元素，也就是训练数据，data[-1:]表示最后一个元素，也就是对应的标签
         """
-        for data in TRAINING_SET:
+        for data in data_set:
             yield data[:-1], data[-1:]
-
     return reader
 
 
-# 测试数据集
+# 获取训练数据集
+def train():
+    """
+    定义一个reader来获取训练数据集及其标签
+
+    Args:
+    Return:
+        read_data -- 用于获取训练数据集及其标签的reader
+    """
+    global TRAINING_SET
+
+    return read_data(TRAINING_SET)
+
+
+# 获取测试数据集
 def test():
     """
     定义一个reader来获取测试数据集及其标签
 
     Args:
     Return:
-        reader -- 用于获取测试数据集及其标签的reader
+        read_data -- 用于获取测试数据集及其标签的reader
     """
     global TEST_SET
 
-    def reader():
-        """
-        一个reader
-        Args:
-        Return:
-            data[:-1], data[-1:] -- 使用yield返回生成器(generator)，
-                    data[:-1]表示前n-1个元素，也就是测试数据，data[-1:]表示最后一个元素，也就是对应的标签
-        """
-        for data in TEST_SET:
-            yield data[:-1], data[-1:]
+    return read_data(TEST_SET)
 
-    return reader
+
+# 获取data，服务于get_train_data()和get_test_data()
+def get_data(data_creator):
+    """
+    使用参数data_creator来获取测试数据
+
+    Args:
+        data_creator -- 数据来源,可以是train()或者test()
+    Return:
+        result -- 包含测试数据(image)和标签(label)的python字典
+    """
+    data_creator = data_creator
+    data_image = []
+    data_label = []
+
+    for item in data_creator():
+        data_image.append((item[0],))
+        data_label.append(item[1])
+
+    result = {
+        "image": data_image,
+        "label": data_label
+    }
+
+    return result
 
 
 # 获取train_data
@@ -114,22 +139,9 @@ def get_train_data():
 
     Args:
     Return:
-        result -- 包含训练数据(image)和标签(label)的python字典
+        get_data(train()) -- 包含训练数据(image)和标签(label)的python字典
     """
-    train_data_creator = train()
-    train_data_image = []
-    train_data_label = []
-
-    for item in train_data_creator():
-        train_data_image.append((item[0],))
-        train_data_label.append(item[1])
-
-    result = {
-        "image": train_data_image,
-        "label": train_data_label
-    }
-
-    return result
+    return get_data(train())
 
 
 # 获取test_data
@@ -139,22 +151,32 @@ def get_test_data():
 
     Args:
     Return:
-        result -- 包含测试数据(image)和标签(label)的python字典
+        get_data(test()) -- 包含测试数据(image)和标签(label)的python字典
     """
-    test_data_creator = test()
-    test_data_image = []
-    test_data_label = []
+    return get_data(test())
 
-    for item in test_data_creator():
-        test_data_image.append((item[0],))
-        test_data_label.append(item[1])
 
-    result = {
-        "image": test_data_image,
-        "label": test_data_label
-    }
+# 计算准确度，服务于train_accuracy()和test_accuracy()
+def calc_accuracy(probs, data):
+    """
+    根据数据集来计算准确度accuracy
 
-    return result
+    Args:
+        probs -- 数据集的预测结果，调用paddle.infer()来获取
+        data -- 数据集
+
+    Return:
+        calc_accuracy -- 训练准确度
+    """
+    right = 0
+    total = len(data['label'])
+    for i in range(len(probs)):
+        if float(probs[i][0]) > 0.5 and data['label'][i] == 1:
+            right += 1
+        elif float(probs[i][0]) < 0.5 and data['label'][i] == 0:
+            right += 1
+    accuracy = (float(right) / float(total)) * 100
+    return accuracy
 
 
 # 训练集准确度
@@ -167,18 +189,9 @@ def train_accuracy(probs_train, train_data):
         train_data -- 训练数据集
 
     Return:
-        train_accuracy -- 训练准确度train_accuracy
+        calc_accuracy -- 训练准确度
     """
-    train_right = 0
-    train_total = len(train_data['label'])
-    for i in range(len(probs_train)):
-        if float(probs_train[i][0]) > 0.5 and train_data['label'][i] == 1:
-            train_right += 1
-        elif float(probs_train[i][0]) < 0.5 and train_data['label'][i] == 0:
-            train_right += 1
-    train_accuracy = (float(train_right) / float(train_total)) * 100
-
-    return train_accuracy
+    return calc_accuracy(probs_train, train_data)
 
 
 # 测试集准确度
@@ -191,48 +204,39 @@ def test_accuracy(probs_test, test_data):
         test_data -- 测试数据集
 
     Return:
-        test_accuracy -- 测试准确度test_accuracy
+        calc_accuracy -- 测试准确度
     """
-    test_right = 0
-    test_total = len(test_data['label'])
-    for i in range(len(probs_test)):
-        if float(probs_test[i][0]) > 0.5 and test_data['label'][i] == 1:
-            test_right += 1
-        elif float(probs_test[i][0]) < 0.5 and test_data['label'][i] == 0:
-            test_right += 1
-    test_accuracy = (float(test_right) / float(test_total)) * 100
 
-    return test_accuracy
+    return calc_accuracy(probs_test, test_data)
 
-# 搭建神经网络结构
-def netconfig():
+
+# 预测
+def infer(y_predict, parameters):
     """
-        搭建浅层神经网络结构
+    预测并输出模型准确率
 
-        Args:
+    Args:
+        y_predict -- 输出层，DATADIM维稠密向量
+        parameters -- 训练完成的模型参数
 
-        Return:
-            image -- 输入层，DATADIM维稠密向量
-            y_predict -- 输出层，Sigmoid作为激活函数
-            y_label -- 标签数据，1维稠密向量
+    Return:
     """
-    # 输入层，paddle.layer.data表示数据层,name=’image’：名称为image,
-    # type=paddle.data_type.dense_vector(DATADIM)：数据类型为DATADIM维稠密向量
-    image = paddle.layer.data(
-        name='image', type=paddle.data_type.dense_vector(DATADIM))
-    h1 = paddle.layer.fc(
-        input=image, size=4, act=paddle.activation.Tanh())
-    # 输出层，paddle.layer.fc表示全连接层，input=image: 该层输入数据为image
-    # size=1：神经元个数，act=paddle.activation.Sigmoid()：激活函数为Sigmoid()
-    y_predict = paddle.layer.fc(
-        input=h1, size=1, act=paddle.activation.Sigmoid())
+    # 获取测试数据和训练数据，用来验证模型准确度
+    train_data = get_train_data()
+    test_data = get_test_data()
 
-    # 数据层，paddle.layer.data表示数据层，name=’label’：名称为label
-    # type=paddle.data_type.dense_vector(1)：数据类型为1维稠密向量
-    y_label = paddle.layer.data(
-        name='label', type=paddle.data_type.dense_vector(1))
+    # 根据train_data和test_data预测结果，output_layer表示输出层，parameters表示模型参数，input表示输入的测试数据
+    probs_train = paddle.infer(
+        output_layer=y_predict, parameters=parameters, input=train_data['image']
+    )
+    probs_test = paddle.infer(
+        output_layer=y_predict, parameters=parameters, input=test_data['image']
+    )
 
-    return image, y_predict, y_label
+    # 计算train_accuracy和test_accuracy
+    print("train_accuracy: {} %".format(train_accuracy(probs_train, train_data)))
+    print("test_accuracy: {} %".format(test_accuracy(probs_test, test_data)))
+
 
 # 展示模型训练曲线
 def plot_costs(costs):
@@ -252,21 +256,39 @@ def plot_costs(costs):
     plt.savefig('costs.png')
 
 
-def main():
+# 配置网络结构
+def netconfig():
     """
-    定义神经网络结构，训练、预测、检验准确率并打印学习曲线
+    配置网络结构
     Args:
     Return:
+        image -- 输入层，DATADIM维稠密向量
+        y_predict -- 输出层，Sigmoid作为激活函数
+        y_label -- 标签数据，1维稠密向量
+        cost -- 损失函数
+        parameters -- 模型参数
+        optimizer -- 优化器
+        feeding -- 数据映射，python字典
     """
-    global DATADIM
-    # 初始化，设置是否使用gpu，trainer数量
-    paddle.init(use_gpu=False, trainer_count=1)
+    # 输入层，paddle.layer.data表示数据层,name=’image’：名称为image,
+    # type=paddle.data_type.dense_vector(DATADIM)：数据类型为DATADIM维稠密向量
+    image = paddle.layer.data(
+        name='image', type=paddle.data_type.dense_vector(DATADIM))
 
-    # 载入数据
-    load_data()
+    # 隐藏层，paddle.layer.fc表示全连接层，input=image: 该层输入数据为image
+    # size=4：神经元个数，act=paddle.activation.Tanh()：激活函数为Tanh()
+    h1 = paddle.layer.fc(
+        input=image, size=4, act=paddle.activation.Tanh())
 
-    #配置网络结构
-    image, y_predict, y_label = netconfig()
+    # 输出层，paddle.layer.fc表示全连接层，input=h1: 该层输入数据为h1
+    # size=1：神经元个数，act=paddle.activation.Sigmoid()：激活函数为Sigmoid()
+    y_predict = paddle.layer.fc(
+        input=h1, size=1, act=paddle.activation.Sigmoid())
+
+    # 标签数据，paddle.layer.data表示数据层，name=’label’：名称为label
+    # type=paddle.data_type.dense_vector(1)：数据类型为1维稠密向量
+    y_label = paddle.layer.data(
+        name='label', type=paddle.data_type.dense_vector(1))
 
     # 定义成本函数为交叉熵损失函数multi_binary_label_cross_entropy_cost
     cost = paddle.layer.multi_binary_label_cross_entropy_cost(input=y_predict, label=y_label)
@@ -282,10 +304,32 @@ def main():
         'image': 0,
         'label': 1}
 
+    data = [image, y_predict, y_label, cost, parameters, optimizer, feeding]
+
+    return data
+
+
+def main():
+    """
+    定义神经网络结构，训练、预测、检验准确率并打印学习曲线
+    Args:
+    Return:
+    """
+    global DATADIM
+
+    # 初始化，设置是否使用gpu，trainer数量
+    paddle.init(use_gpu=False, trainer_count=1)
+
+    # 载入数据
+    load_data()
+
+    # 配置网络结构
+    image, y_predict, y_label, cost, parameters, optimizer, feeding = netconfig()
+
     # 记录成本cost
     costs = []
 
-    # 事件处理
+    # 处理事件
     def event_handler(event):
         """
         事件处理器，可以根据训练过程的信息作相应操作
@@ -298,8 +342,8 @@ def main():
             if event.pass_id % 100 == 0:
                 print("Pass %d, Batch %d, Cost %f" % (event.pass_id, event.batch_id, event.cost))
                 costs.append(event.cost)
-                # with open('params_pass_%d.tar' % event.pass_id, 'w') as f:
-                #     parameters.to_tar(f)
+                with open('params_pass_%d.tar' % event.pass_id, 'w') as f:
+                    parameters.to_tar(f)
 
     # 构造trainer,配置三个参数cost、parameters、update_equation，它们分别表示成本函数、参数和更新公式。
     trainer = paddle.trainer.SGD(
@@ -320,25 +364,12 @@ def main():
             batch_size=256),
         feeding=feeding,
         event_handler=event_handler,
-        num_passes=2000)
+        num_passes=10000)
 
-    # 获取测试数据和训练数据，用来验证模型准确度
-    train_data = get_train_data()
-    test_data = get_test_data()
+    # 预测
+    infer(y_predict, parameters)
 
-    # 根据train_data和test_data预测结果，output_layer表示输出层，parameters表示模型参数，input表示输入的测试数据
-    probs_train = paddle.infer(
-        output_layer=y_predict, parameters=parameters, input=train_data['image']
-    )
-    probs_test = paddle.infer(
-        output_layer=y_predict, parameters=parameters, input=test_data['image']
-    )
-
-    # 计算train_accuracy和test_accuracy
-    print("train_accuracy: {} %".format(train_accuracy(probs_train, train_data)))
-    print("test_accuracy: {} %".format(test_accuracy(probs_test, test_data)))
-
-    #绘制成本函数折线图
+    # 展示学习曲线
     plot_costs(costs)
 
 
