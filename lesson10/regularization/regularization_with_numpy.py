5fed43a5e9ca9603d435ff3acd278272c3172d03
Melon-Water <kedou1993@163.com>
2018-03-09 20:27:38 +0800

    fix style problems

--- lesson10/regularization/regularization_with_numpy.py
+++ lesson10/regularization/regularization_with_numpy.py
@@ -6,25 +6,24 @@ Date:    2017/11/29
 """
 
 import matplotlib.pyplot as plt
-import reg_utils
+import utils
 
 # 绘图相关设置
-plt.rcParams['figure.figsize'] = (7.0, 4.0)
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
 
 def model(X, Y, model_name="_without_regularization",
-          learning_rate=0.3, num_iterations=30000, print_cost=True,
+          learning_rate=0.3, iteration_num=30000, print_cost=True,
           lambd=0, keep_prob=1):
     """
     实现一个3层神经网络：三层网络的激活函数分别为RELU，RELU，SIGMOID
     Args:
         X: 输入数据，尺寸大小为(输入尺寸, 样本数量)
-        Y: 数据真实标签，1表示蓝点（进攻方队员），0表示红点（防守方队员），尺寸大小为(输出尺寸, 样本数量)
+        Y: 数据真实标签，1表示蓝点，0表示红点，尺寸大小为(输出尺寸, 样本数量)
         model_name: model的名字，用于绘图输出，例如可用”_without_regularization“表示模型不采用任何正则化方式
         learning_rate: 学习步长
-        num_iterations: 迭代次数
+        iteration_num: 迭代次数
         print_cost: 打印flag，如果为真，则每10000次迭代输出一个目标函数cost值
         lambd: L2正则化超参数
         keep_prob: dropout操作超参数，取值范围为(0,1]，表示在dropout过程中一个神经元保持激活的概率
@@ -32,34 +31,35 @@ def model(X, Y, model_name="_without_regularization",
         parameters: 模型的参数，可用于预测
     """
 
-    grads = {}
-    costs = []  # 用于记录cost值
+
     m = X.shape[1]  # 样本数量
     layers_dims = [X.shape[0], 20, 3, 1]
+    gradients = {}
+    costs = []  # 用于记录cost值
 
     # 初始化参数
-    parameters = reg_utils.initialize_parameters(layers_dims)
+    parameters = utils.initialize(layers_dims)
 
     # 梯度下降循环
 
-    for i in range(0, num_iterations):
+    for i in range(0, iteration_num):
 
         # 正向传播
         # 不使用dropout
         if keep_prob == 1:
-            a3, cache = reg_utils.forward_propagation(X, parameters)
+            a3, cache = utils.forward_calculate(X, parameters)
         # 使用dropout
         elif keep_prob < 1:
-            a3, cache = reg_utils.forward_propagation_with_dropout(
+            a3, cache = utils.forward_calculate_with_dropout(
                 X, parameters, keep_prob)
 
         # 计算目标函数cost值
         # 不使用L2正则化
         if lambd == 0:
-            cost = reg_utils.compute_cost(a3, Y)
+            cost = utils.calculate_cost(a3, Y)
         # 使用L2正则化
         else:
-            cost = reg_utils.compute_cost_with_regularization(
+            cost = utils.calculate_cost_with_regularization(
                 a3, Y, parameters, lambd)
 
         assert (lambd == 0 or keep_prob == 1)
@@ -68,19 +68,19 @@ def model(X, Y, model_name="_without_regularization",
         # 反向传播
         # 不使用正则化
         if lambd == 0 and keep_prob == 1:
-            grads = reg_utils.backward_propagation(X, Y, cache)
+            gradients = utils.backward_calculate(X, Y, cache)
         # 使用L2正则化
         elif lambd != 0:
-            grads = reg_utils.backward_propagation_with_regularization(
+            gradients = utils.backward_calculate_with_regularization(
                 X, Y, cache, lambd)
         # 使用dropout
         elif keep_prob < 1:
-            grads = reg_utils.backward_propagation_with_dropout(
+            gradients = utils.backward_calculate_with_dropout(
                 X, Y, cache, keep_prob)
 
         # 更新参数
-        parameters = reg_utils.update_parameters(
-            parameters, grads, learning_rate)
+        parameters = utils.update(
+            parameters, gradients, learning_rate)
 
         # 如果绘图标志位print_cost为真，则每10000次迭代输出一个目标函数cost值
         if print_cost and i % 10000 == 0:
@@ -106,24 +106,24 @@ def main():
     """
 
     # 读入数据集
-    train_X, train_Y, test_X, test_Y = reg_utils.load_2D_dataset("data")
+    train_X, train_Y, test_X, test_Y = utils.load_dataset("data")
 
     # 训练不使用正则化的原始模型
     model_name = "_without_regularization"
     parameters = model(train_X, train_Y, model_name)
     # 在训练集和测试集上的进行预测并输出准确率
     print "On the training set:"
-    predictions_train = reg_utils.predict(train_X, train_Y, parameters)
+    predictions_train = utils.predict(train_X, train_Y, parameters)
     print "On the test set:"
-    predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+    predictions_test = utils.predict(test_X, test_Y, parameters)
     # 绘制边界预测结果图
     plt.clf()
     plt.title("Model without regularization")
     axes = plt.gca()
     axes.set_xlim([-0.75, 0.40])
     axes.set_ylim([-0.75, 0.65])
-    reg_utils.plot_decision_boundary(
-        lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y,
+    utils.plot_boundary(
+        lambda x: utils.predict_decision(parameters, x.T), train_X, train_Y,
         "decision_boundary" + model_name + ".png")
 
     # 训练使用L2正则化的模型
@@ -131,17 +131,17 @@ def main():
     parameters = model(train_X, train_Y, model_name, lambd=0.7)
     # 在训练集和测试集上的进行预测并输出准确率
     print "On the train set:"
-    predictions_train = reg_utils.predict(train_X, train_Y, parameters)
+    predictions_train = utils.predict(train_X, train_Y, parameters)
     print "On the test set:"
-    predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+    predictions_test = utils.predict(test_X, test_Y, parameters)
     # 绘制边界预测结果图
     plt.clf()
     plt.title("Model with L2-regularization")
     axes = plt.gca()
     axes.set_xlim([-0.75, 0.40])
     axes.set_ylim([-0.75, 0.65])
-    reg_utils.plot_decision_boundary(
-        lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y,
+    utils.plot_boundary(
+        lambda x: utils.predict_decision(parameters, x.T), train_X, train_Y,
         "decision_boundary" + model_name + ".png")
 
     # 训练使用dropout的模型
@@ -149,17 +149,17 @@ def main():
     parameters = model(train_X, train_Y, model_name, keep_prob=0.86)
     # 在训练集和测试集上的进行预测并输出准确率
     print "On the train set:"
-    predictions_train = reg_utils.predict(train_X, train_Y, parameters)
+    predictions_train = utils.predict(train_X, train_Y, parameters)
     print "On the test set:"
-    predictions_test = reg_utils.predict(test_X, test_Y, parameters)
+    predictions_test = utils.predict(test_X, test_Y, parameters)
     # 绘制边界预测结果图
     plt.clf()
     plt.title("Model with dropout")
     axes = plt.gca()
     axes.set_xlim([-0.75, 0.40])
     axes.set_ylim([-0.75, 0.65])
-    reg_utils.plot_decision_boundary(
-        lambda x: reg_utils.predict_dec(parameters, x.T), train_X, train_Y,
+    utils.plot_boundary(
+        lambda x: utils.predict_decision(parameters, x.T), train_X, train_Y,
         "decision_boundary" + model_name + ".png")
 
 
